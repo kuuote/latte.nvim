2cf3a08118321cd0258c5a5a015d3fe49928428e
kuuote <znmxodq1@gmail.com>
2022-11-21 18:07:05 +0000

    improve function snip

--- latte/lua/function.lua
+++ latte/lua/function.lua
@@ -2,16 +2,19 @@ return {
   render = function(params)
     return string.format(
       [[
-function %s(%s)
+%sfunction%s(%s)
 	%s
-end]],
-      params.name,
+end
+]],
+      params.is_local and 'local ' or '',
+      params.name == '' and '' or ' ' .. params.name,
       params.args,
       require('latte').get_cursor_marker()
     )
   end,
   params = [[
 return {
+  is_local = false,
   name = '',
   args = '',
 }
